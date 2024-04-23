3a4b79e6e32dfa01533d72255bc8ab3a684e7c94
Spriha Jha <jha.spriha@gmail.com>
2024-04-23 01:40:58 -0400

    Removed metadata from main page

--- user-interface/app/layout.tsx
+++ user-interface/app/layout.tsx
@@ -1,28 +1,8 @@
 import "@/styles/globals.css";
-import { Metadata } from "next";
-import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Providers } from "./providers";
 import { Navbar } from "@/components/navbar";
-import { Link } from "@nextui-org/link";
 import clsx from "clsx";
-
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-};
 
 export default function RootLayout({
   children,
@@ -38,10 +18,10 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
+        <Providers>
           <div className="relative flex flex-col h-screen w-screen bg-gradient-to-r from-indigo-100">
             <Navbar />
-            <main className="container mx-auto h-full flex-grow">
+            <main className="container mx-auto h-screen flex-grow">
               {children}
             </main>
             {/* TODO: Footer */}
